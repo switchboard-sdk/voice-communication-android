1c4172cec2cffcc30055e76b179013af9c75552b
Ivan Nador <ivan.nador@synervoz.com>
2023-08-23 19:04:31 +0200

    Added voice communication app skeleton

---
+++ VoiceCommunicationApp/app/src/main/java/com/synervoz/voicecommunicationapp/Config.kt
@@ -0,0 +1,9 @@
+package com.synervoz.voicecommunicationapp
+
+object Config {
+    val clientID = "Synervoz"
+    val clientSecret = "VocalFXChainsApp"
+    val agoraAppID = ""
+
+    val switchboardAPIBaseURL = "https://api-dev.synervoz.com/"
+}
